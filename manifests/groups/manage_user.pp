c03dee84d0f3910e4e68f41251b12ea36395b5bd
mh <mh@immerda.ch>
2011-05-24 22:11:31 +0200

    fixing adding user to an empty group based on http://watzmann.net/blog/2010/05/augeas-append-users.html

--- manifests/groups/manage_user.pp
+++ manifests/groups/manage_user.pp
@@ -15,7 +15,8 @@ define user::groups::manage_user(
     }
     if ($ensure == 'present'){
         Augeas["manage_${real_user}_in_group_${group}"]{
-            changes => [ "set ${group}/user[last()+1] ${real_user}" ],
+            changes => [ "ins user after ${group}/*[self::gid or self::user][last()]",
+                         "set ${group}/user[last()] ${real_user}" ],
             onlyif => "match ${group}/*[../user='${real_user}'] size == 0"
         }
     } else {
